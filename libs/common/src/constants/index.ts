7ea538eb5e83261e1ed2de951eab4c2f6ec21571
Arkadipta <dasarkadipta@gmail.com>
2024-11-20 22:49:00 +0530

    fix: add kafka service token

--- libs/common/src/constants/index.ts
+++ libs/common/src/constants/index.ts
@@ -1,10 +1,8 @@
-export const TOPICS: Record<
-  string,
-  Record<'CREATED' | 'UPDATED' | 'DELETED', string>
-> = {
+export const TOPICS = {
   USER: {
     CREATED: 'user.created',
     UPDATED: 'user.updated',
     DELETED: 'user.deleted',
   },
-};
+} as const;
+export const KAFKA_SERVICE_TOKEN = 'KAFKA_SERVICE' as const;
