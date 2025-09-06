ca95ad051b5669e9a55889eafe8a2875d8e1de27
Arkadipta <dasarkadipta@gmail.com>
2025-09-06 15:31:29 +0530

    docs(changeset): fix: debug chalk issue

--- libs/common/src/modules/logger/logger.service.ts
+++ libs/common/src/modules/logger/logger.service.ts
@@ -1,8 +1,10 @@
 import { ConsoleLogger } from '@nestjs/common';
-import chalk from 'chalk';
+import * as chalk from 'chalk';
 
 export class LoggerService extends ConsoleLogger {
   override debug(message: unknown, context?: unknown): void {
-    super.debug(chalk.blue(message), context);
+    console.log(chalk);
+
+    super.debug(new chalk.Chalk().blue(message), context);
   }
 }
